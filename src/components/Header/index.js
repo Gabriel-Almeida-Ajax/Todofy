ccb43b58759280c169fdeb8f795d4346bc4ae1cb
GABRIEL SANTOS LIMA <lima.gabriel@LELLO.CORP>
2021-12-09 18:35:40 -0300

    ajuste

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,11 @@ import * as S from './styles';
 
 export function Header() {
     return (
-        <S.Container>
-            <h1>To do List</h1>
-        </S.Container>
+        <a href='https://github.com/Gabriel-Almeida-Ajax/monkeysfy'>
+            <S.Container>
+                <h1>To do List</h1>
+            </S.Container>
+        </a>
     );
 }
 
